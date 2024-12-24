c6415ac755257b6deb799ae9bae66aad2fc2f374
Eduardo Melo <eduardoddmg@gmail.com>
2024-12-24 20:42:52 -0300

    Ajuste no rodapé

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <section>
       <div className="container">
         <footer>
-          <div className="mt-24 flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
+          <div className="flex flex-col justify-between items-center gap-4 border-t py-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
             <p>© 2024 Eduardo Melo. Todos os direitos reservados.</p>
           </div>
         </footer>
