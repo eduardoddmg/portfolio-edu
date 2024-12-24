972086041cd57beb31d8f3c3f722f7560d25325e
Eduardo Melo <eduardoddmg@gmail.com>
2024-12-24 20:42:38 -0300

    Ajuste na responsividade

--- app/projetos/page.tsx
+++ app/projetos/page.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
   const { tagline, title, description, buttonText, items } = projectsData;
 
   return (
-    <section className="py-32">
+    <section>
       <div className="container flex flex-col items-center gap-16 lg:px-16">
         <div className="text-center">
           <p className="mb-6 text-xs font-medium uppercase tracking-wider">
